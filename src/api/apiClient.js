b344c49b850dac6c998a6001f87bc688bcab3948
KaterinaSSlavova <k.slavova@student.fontys.nl>
2026-04-12 15:24:52 +0200

    fixing frontend auth connection

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -4,7 +4,7 @@ const api = axios.create({baseURL: import.meta.env.VITE_API_BASE_URL});
 api.interceptors.request.use((config) => {
    const token = localStorage.getItem("token");
 
-   if(token){
+   if(token && !config.url?.includes("/auth")){
       config.headers.Authorization = `Bearer ${token}`;
    }
 
